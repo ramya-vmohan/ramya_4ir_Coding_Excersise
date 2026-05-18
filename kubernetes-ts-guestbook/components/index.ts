02506aecb93fd2e79b755d016cd8eab3d9f5a1f6
Ramya Mohan <67153763+ramya-vmohan@users.noreply.github.com>
2026-05-18 01:15:37 -0400

    Update index.ts

--- kubernetes-ts-guestbook/components/index.ts
+++ kubernetes-ts-guestbook/components/index.ts
@@ -1,24 +1,26 @@
+// Copyright 2016-2025, Pulumi Corporation.  All rights reserved.
+
 import * as pulumi from "@pulumi/pulumi";
 import * as k8sjs from "./k8sjs";
+import "./monitoring";
+const config = new pulumi.Config();
 
-let config = new pulumi.Config();
-
-let redisMaster = new k8sjs.ServiceDeployment("redis-master", {
-    image: "k8s.gcr.io/redis:e2e",
-    ports: [6379]
+const redisLeader = new k8sjs.ServiceDeployment("redis-leader", {
+    image: "redis",
+    ports: [6379],
 });
 
-let redisReplica = new k8sjs.ServiceDeployment("redis-replica", {
-    image: "gcr.io/google_samples/gb-redisslave:v1",
-    ports: [6379]
+const redisReplica = new k8sjs.ServiceDeployment("redis-replica", {
+    image: "pulumi/guestbook-redis-replica",
+    ports: [6379],
 });
 
-let frontend = new k8sjs.ServiceDeployment("frontend", {
+const frontend = new k8sjs.ServiceDeployment("frontend", {
     replicas: 3,
-    image: "gcr.io/google-samples/gb-frontend:v4",
+    image: "pulumi/guestbook-php-redis",
     ports: [80],
     allocateIpAddress: true,
-    isMinikube: config.getBoolean("isMinikube")
+    isMinikube: config.getBoolean("isMinikube"),
 });
 
 export let frontendIp = frontend.ipAddress;
