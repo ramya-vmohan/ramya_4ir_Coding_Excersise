52537be67425ab3b568a201a9c534ccba987fcc5
Ramya Mohan <67153763+ramya-vmohan@users.noreply.github.com>
2026-05-18 01:16:21 -0400

    Update k8sjs.ts

--- kubernetes-ts-guestbook/components/k8sjs.ts
+++ kubernetes-ts-guestbook/components/k8sjs.ts
@@ -1,3 +1,5 @@
+// Copyright 2016-2025, Pulumi Corporation.  All rights reserved.
+
 import * as k8s from "@pulumi/kubernetes";
 import * as k8stypes from "@pulumi/kubernetes/types/input";
 import * as pulumi from "@pulumi/pulumi";
@@ -35,6 +37,7 @@ export class ServiceDeployment extends pulumi.ComponentResource {
 
         this.service = new k8s.core.v1.Service(name, {
             metadata: {
+                name: name,
                 labels: this.deployment.metadata.labels,
             },
             spec: {
@@ -42,14 +45,16 @@ export class ServiceDeployment extends pulumi.ComponentResource {
                 selector: this.deployment.spec.template.metadata.labels,
                 // Minikube does not implement services of type `LoadBalancer`; require the user to specify if we're
                 // running on minikube, and if so, create only services of type ClusterIP.
-                type: args.allocateIpAddress ? (args.isMinikube ? "ClusterIP" : "LoadBalancer") : undefined,
+                //type: args.allocateIpAddress ? (args.isMinikube ? "ClusterIP" : "LoadBalancer") : undefined,
+		type: args.allocateIpAddress ? "NodePort" : undefined,
+
             },
         }, { parent: this });
 
         if (args.allocateIpAddress) {
             this.ipAddress = args.isMinikube ?
-                this.service.spec.apply(spec => spec.clusterIP) :
-                this.service.status.apply(status => status.loadBalancer.ingress[0].ip);
+                this.service.spec.clusterIP :
+                this.service.status.loadBalancer.ingress[0].ip;
         }
     }
 }
